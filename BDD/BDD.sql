63955c28e3e702059b63a843711da3d9ca849fb9
leonelparrales22 <dparrales@est.ups.edu.ec>
2020-06-21 21:11:44 -0500

    Update BDD.sql

--- BDD/BDD.sql
+++ BDD/BDD.sql
@@ -28,10 +28,10 @@ CREATE TABLE VENTAS (
 CREATE TABLE FACTURA (
                 id_factura VARCHAR(36) NOT NULL,
                 codigo_venta VARCHAR(36) NOT NULL,
-                subtotal DECIMAL(5,2) NOT NULL,
-                descuento DECIMAL(5,2) NOT NULL,
-                iva DECIMAL(5,2) NOT NULL,
-                total DECIMAL(5,2) NOT NULL,
+                subtotal DECIMAL(7,2) NOT NULL,
+                descuento DECIMAL(7,2) NOT NULL,
+                iva DECIMAL(7,2) NOT NULL,
+                total DECIMAL(7,2) NOT NULL,
                 observaciones VARCHAR(1000) NOT NULL,
                 PRIMARY KEY (id_factura)
 );
@@ -42,7 +42,7 @@ CREATE TABLE CELULARES (
                 nombre_celular VARCHAR(150) NOT NULL,
                 marca_celular VARCHAR(50),
                 stock_celular INT NOT NULL,
-                precio_celular DECIMAL(5,2) NOT NULL,
+                precio_celular DECIMAL(7,2) NOT NULL,
                 PRIMARY KEY (id_celular)
 );
 
@@ -107,6 +107,43 @@ INSERT INTO celulares VALUES("6514f137-ab23-4e15-8ca9-0c0ee7c6d880","iPhone 8 25
 
 
 INSERT INTO celulares VALUES("2c985fd8-64db-42ee-8e9c-4adcb002e3ef","iPhone 8 Plus 64GB","APPLE",10,577);
-INSERT INTO celulares VALUES("5b2a3d39-fc57-4a67-bf15-a8f9d10aa335","-iPhone 8 Plus 128GB","APPLE",5,617);
+INSERT INTO celulares VALUES("5b2a3d39-fc57-4a67-bf15-a8f9d10aa335","iPhone 8 Plus 128GB","APPLE",5,617);
 INSERT INTO celulares VALUES("7fe0f0de-10b7-4d80-8163-07a61b0512fa","iPhone 8 Plus 256GB","APPLE",7,647);
+
+
+
+
+
+
+INSERT INTO celulares VALUES("30b15f34-f40a-41b3-862c-4c0050e1784c","A01 16GB","SAMSUNG",15,139);
+INSERT INTO celulares VALUES("8b1dce9b-637f-431a-b746-dc3083b0edfb","A10s 32GB","SAMSUNG",4,159);
+INSERT INTO celulares VALUES("cae0fa0c-c2af-45fd-8788-1ef81b4a5bc0","A11 32GB","SAMSUNG",7,175);
+INSERT INTO celulares VALUES("0b3c378d-d6d0-4dbf-bd10-b3eccf2976e2","A20s 32GB","SAMSUNG",5,185);
+INSERT INTO celulares VALUES("f0d481fc-43a1-4ab6-8a53-fd2d8ec0cda9","A21s 32GB","SAMSUNG",2,210);
+INSERT INTO celulares VALUES("38f2e34b-a727-4c9d-b4f5-6c6c28ea5cdb","A30s 64GB","SAMSUNG",8,240);
+INSERT INTO celulares VALUES("b07a4af2-edff-4009-9685-80e3bf230cef","A31 64GB","SAMSUNG",12,280);
+INSERT INTO celulares VALUES("165d9a74-b990-4bd5-881c-d4021309a9ce","A51 128GB","SAMSUNG",13,330);
+INSERT INTO celulares VALUES("bf60132e-15c8-4f89-a6e0-1ab7bf1dce87","A71 128GB","SAMSUNG",4,435);
+INSERT INTO celulares VALUES("6e1998fa-3d84-4738-ac94-134c8ecfbed2","S20 plus","SAMSUNG",2,1010);
+INSERT INTO celulares VALUES("9ec97fce-7041-4293-a054-612424ed2705","NOTE 10 LITE","SAMSUNG",2,639.49);
+INSERT INTO celulares VALUES("40839f41-624c-42f8-bfa0-f4f51509a115","START DUOS","SAMSUNG",7,60);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
