54bc36f1917ab49c360cb02da440c8f756735296
dgonzalezq2 <52825682+dgonzalezq2@users.noreply.github.com>
2020-07-03 20:18:33 -0500

    actualizacion conexion_base.js

--- Caja_Registradora_Java/Login_Caja/conexion_base.js
+++ Caja_Registradora_Java/Login_Caja/conexion_base.js
@@ -1,9 +1,11 @@
 var mysql = require('mysql');
+//Recordar cambiar la clave de hipermedial a '' cuando trabaje en windows
+
 var conexion = mysql.createConnection({
     host: 'localhost',
     database: 'bdd',
     user: 'root',
-    password: '',
+    password: 'hipermedial',
 });
 
 conexion.connect(function(err) {
@@ -14,7 +16,8 @@ conexion.connect(function(err) {
     console.log('Conectado con el identificador ' + conexion.threadId);
 });
 
-conexion.query('SELECT * FROM detalle_venta', function(error, results, fields) {
+conexion.query('SELECT * FROM VENTAS', function(error, results, fields) {
+
     if (error)
         throw error;
 
