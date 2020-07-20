a81dc3a1bbe12590803ec2c083e7e30783d6fbf9
leonelparrales22 <dparrales@est.ups.edu.ec>
2020-07-20 12:54:09 -0500

    Formulario de añadir celular

--- proyecto-galvez/src/components/Celulares.vue
+++ proyecto-galvez/src/components/Celulares.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="page-wrapper">
+  <div class="page-wrapper" id="wrapper">
     <div class="container-fluid">
       <div class="row">
         <div class="col-12">
@@ -11,6 +11,7 @@
                 </div>
                 <div class="col-sm-6 alinear-derecha">
                   <a
+                    id="boton1"
                     href="#addEmployeeModal"
                     class="btn btn-success alinear-derecha"
                     data-toggle="modal"
@@ -55,12 +56,18 @@
                       </thead>
                       <tbody>
                         <tr v-for="celular in celulares" :key="celular.id_celular">
-                          <td>{{celular.nombre_celular}}</td>
-                          <td>{{celular.marca_celular}}</td>
-                          <td>{{celular.stock_celular}}</td>
-                          <td>{{celular.precio_celular | moneda}}</td>
+                          <td>{{ celular.nombre_celular }}</td>
+                          <td>{{ celular.marca_celular }}</td>
+                          <td>{{ celular.stock_celular }}</td>
+                          <td>{{ celular.precio_celular | moneda }}</td>
                           <td class="centrar-horizontal">
-                            <a href="#editEmployeeModal" class="edit" data-toggle="modal">
+                            <a
+                              href="#editEmployeeModal"
+                              class="edit"
+                              data-toggle="modal"
+                              data-book-id="LEONELPAPIPARRALES"
+                              @click="editar_celular(celular.id_celular)"
+                            >
                               <div class="hover01">
                                 <div>
                                   <figure class="figure">
@@ -76,8 +83,8 @@
                     <div class="clearfix">
                       <div class="hint-text">
                         Mostrando
-                        <b>{{celulares.length}}</b> de
-                        <b>{{total}}</b> registros
+                        <b>{{ celulares.length }}</b> de
+                        <b>{{ total }}</b> registros
                       </div>
                       <ul class="pagination" v-if="paginado">
                         <li class="page-item disabled">
@@ -88,13 +95,13 @@
                           :key="n"
                           class="page-item"
                           :class="{
-                          active: n === pagenow
-                        }"
+                            active: n === pagenow,
+                          }"
                         >
                           <router-link
-                            :to="{ name: 'celulares', params: { pagenow: n }}"
+                            :to="{ name: 'celulares', params: { pagenow: n } }"
                             class="page-link"
-                          >{{n}}</router-link>
+                          >{{ n }}</router-link>
                         </li>
                         <li class="page-item">
                           <a href="#" class="page-link">Después</a>
@@ -105,10 +112,11 @@
                 </div>
               </div>
               <!--Add Modal HTML -->
+              <!-- :class="{ in: modalShown }" -->
               <div id="addEmployeeModal" class="modal fade">
                 <div class="modal-dialog">
                   <div class="modal-content">
-                    <form>
+                    <form @submit.prevent="mostrarCelular()">
                       <div class="modal-header">
                         <h4 class="modal-title">Añadir Celular</h4>
                         <button
@@ -121,29 +129,71 @@
                       <div class="modal-body">
                         <div class="form-group">
                           <label>Modelo</label>
-                          <input type="text" class="form-control" required />
+                          <input
+                            type="text"
+                            class="form-control"
+                            v-model="celular.modelo"
+                            name="nombre_celular"
+                          />
+                          <div
+                            v-if="submitted && !$v.celular.modelo.required"
+                          >Este campo es requerido</div>
                         </div>
                         <div class="form-group">
                           <label>Marca</label>
-                          <input type="email" class="form-control" required />
+                          <input
+                            type="text"
+                            class="form-control"
+                            v-model="celular.marca"
+                            name="marca_celular"
+                          />
+                          <div
+                            v-if="submitted && !$v.celular.marca.required"
+                          >Este campo es requerido</div>
                         </div>
                         <div class="form-group">
                           <label>Stock</label>
-                          <textarea class="form-control" required></textarea>
+                          <input
+                            type="number"
+                            class="form-control"
+                            v-model="celular.stock"
+                            name="stock_celular"
+                            min="1"
+                            max="100"
+                          />
+                          <div
+                            v-if="submitted && !$v.celular.stock.required"
+                          >Este campo es requerido</div>
+                          <div
+                            v-if="submitted && $v.celular.stock.required && !$v.celular.stock.between"
+                          >Valores entre 1 y 100</div>
                         </div>
                         <div class="form-group">
                           <label>Precio</label>
-                          <input type="text" class="form-control" required />
+                          <input
+                            type="number"
+                            class="form-control"
+                            v-model="celular.precio"
+                            name="precio_celular"
+                            min="1"
+                            max="5000"
+                          />
+                          <div
+                            v-if="submitted && !$v.celular.precio.required"
+                          >Este campo es requerido</div>
                         </div>
+                        <div
+                          v-if="submitted && $v.celular.precio.required && !$v.celular.precio.between"
+                        >Valores entre 1 y 5000</div>
                       </div>
                       <div class="modal-footer">
                         <input
                           type="button"
                           class="btn btn-default"
                           data-dismiss="modal"
-                          value="Cancel"
+                          value="Cancelar"
                         />
-                        <input type="submit" class="btn btn-success" value="Add" />
+                        <input type="submit" class="btn btn-success" value="Añadir" />
                       </div>
                     </form>
                   </div>
@@ -207,10 +257,35 @@
 <script>
 import axios from "axios";
 import FooterComponent from "./FooterComponent.vue";
+import { required, minLength, between } from "vuelidate/lib/validators";
+import $ from "jquery";
 export default {
   name: "Celulares",
   components: {
     FooterComponent
+  },
+  validations: {
+    submitted: false,
+    celular: {
+      modelo: {
+        required,
+        minLenght: minLength(1)
+      },
+      marca: {
+        required,
+        minLenght: minLength(1)
+      },
+      stock: {
+        required,
+        minLenght: minLength(1),
+        between: between(1, 100)
+      },
+      precio: {
+        required,
+        minLenght: minLength(1),
+        between: between(1, 5000)
+      }
+    }
   },
   mounted() {
     this.pagenow = this.$route.params.pagenow;
@@ -222,28 +297,52 @@ export default {
       }
     }
     this.getCelulares();
+    console.log("kk");
+    $("#editEmployeeModal").on("show.bs.modal", function(e) {
+      //get data-id attribute of the clicked element
+      var bookId = $(e.relatedTarget).data("book-id");
+      console.log("OE", bookId);
+      //populate the textbox
+      $(e.currentTarget)
+        .find('input[name="bookId"]')
+        .val(bookId);
+    });
+    $(document).ready(function() {
+      $("#editEmployeeModal").on("show.bs.modal", function() {
+        alert("Hello World!");
+      });
+    });
   },
   watch: {
     $route() {
-      this.rest = "http://localhost:3100/todos-celulares";
-      this.pagenow = this.$route.params.pagenow;
-      if (this.pagenow != null) {
-        if (this.pagenow > 1) {
-          this.rest += `?desde=${(this.pagenow - 1) * 7 + 1}`;
-        } else {
-          this.rest += `?desde=0`;
-        }
-      }
+      this.actualizar_paginado();
       this.getCelulares();
     }
   },
   data() {
     return {
       rest: "http://localhost:3100/todos-celulares",
+      // Paginacion
       pagenow: null,
       celulares: [],
       paginado: 0,
-      total: 0
+      total: 0,
+      // Formulario
+      submitted: false,
+      celular: {
+        modelo: "",
+        marca: "",
+        stock: "",
+        precio: ""
+      },
+      // Editar
+      editar:{
+        modelo: "",
+        marca: "",
+        stock: "",
+        precio: ""
+      },
+      modalShown: false
     };
   },
   methods: {
@@ -258,6 +357,57 @@ export default {
         .catch(err => {
           console.error(err);
         });
+    },
+    actualizar_paginado() {
+      this.rest = "http://localhost:3100/todos-celulares";
+      this.pagenow = this.$route.params.pagenow;
+      if (this.pagenow != null) {
+        if (this.pagenow > 1) {
+          this.rest += `?desde=${(this.pagenow - 1) * 7 + 1}`;
+        } else {
+          this.rest += `?desde=0`;
+        }
+      }
+    },
+    save() {
+      const modal = document.getElementById("addEmployeeModal");
+      modal.classList.remove("show");
+      modal.setAttribute("aria-hidden", "true");
+      modal.setAttribute("style", "display: none");
+      const modalBackdrops = document.getElementsByClassName("modal-backdrop");
+      document.body.removeChild(modalBackdrops[0]);
+      document.getElementById("addEmployeeModal").click();
+      document.getElementById("wrapper").click();
+      document.getElementById("boton1").blur();
+      document.getElementById("wrapper").click();
+      var body = document.body;
+      body.classList.remove("modal-open");
+      axios
+        .post("http://localhost:3100/insertar-celular", {
+          nombre_celular: this.celular.modelo,
+          marca_celular: this.celular.marca,
+          stock_celular: this.celular.stock,
+          precio_celular: this.celular.precio
+        })
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error.response);
+        });
+      this.actualizar_paginado();
+      this.getCelulares();
+    },
+    mostrarCelular() {
+      this.submitted = true;
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        return false;
+      }
+      this.save();
+    },
+    editar_celular(id) {
+      console.log("dddjd", id);
     }
   }
 };
