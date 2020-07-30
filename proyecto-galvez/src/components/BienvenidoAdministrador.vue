b8600950d116d2697b24470347d670f49760bfa1
leonelparrales22 <dparrales@est.ups.edu.ec>
2020-07-30 10:45:41 -0500

    Cambios

--- proyecto-galvez/src/components/BienvenidoAdministrador.vue
+++ proyecto-galvez/src/components/BienvenidoAdministrador.vue
@@ -7,10 +7,13 @@
           <h2 class="bienvenido">Bienvenido</h2>
         </center>
         <center class="m-t-30">
-          <img src="../assets/img/cajero.jpg" class="img-circle" width="300" />
+          <!-- <img src="../assets/img/cajero.png" class="img-circle" width="300" /> -->
+          <div class="fondo2"></div>
           <br />
           <br />
-          <h4 class="card-title m-t-10 subtitulo">Administrador: {{this.$parent.cedula}}</h4>
+          <h4 class="card-title m-t-10 subtitulo">
+            Administrador: {{ this.$parent.cedula }}
+          </h4>
         </center>
         <div class="right-sidebar">
           <div class="slimscrollright">
@@ -26,22 +29,52 @@
                   <b>With Light sidebar</b>
                 </li>
                 <li>
-                  <a href="javascript:void(0)" data-theme="default" class="default-theme">1</a>
+                  <a
+                    href="javascript:void(0)"
+                    data-theme="default"
+                    class="default-theme"
+                    >1</a
+                  >
                 </li>
                 <li>
-                  <a href="javascript:void(0)" data-theme="green" class="green-theme">2</a>
+                  <a
+                    href="javascript:void(0)"
+                    data-theme="green"
+                    class="green-theme"
+                    >2</a
+                  >
                 </li>
                 <li>
-                  <a href="javascript:void(0)" data-theme="red" class="red-theme">3</a>
+                  <a
+                    href="javascript:void(0)"
+                    data-theme="red"
+                    class="red-theme"
+                    >3</a
+                  >
                 </li>
                 <li>
-                  <a href="javascript:void(0)" data-theme="blue" class="blue-theme">4</a>
+                  <a
+                    href="javascript:void(0)"
+                    data-theme="blue"
+                    class="blue-theme"
+                    >4</a
+                  >
                 </li>
                 <li>
-                  <a href="javascript:void(0)" data-theme="purple" class="purple-theme">5</a>
+                  <a
+                    href="javascript:void(0)"
+                    data-theme="purple"
+                    class="purple-theme"
+                    >5</a
+                  >
                 </li>
                 <li>
-                  <a href="javascript:void(0)" data-theme="megna" class="megna-theme">6</a>
+                  <a
+                    href="javascript:void(0)"
+                    data-theme="megna"
+                    class="megna-theme"
+                    >6</a
+                  >
                 </li>
                 <li class="d-block m-t-30">
                   <b>With Dark sidebar</b>
@@ -51,22 +84,48 @@
                     href="javascript:void(0)"
                     data-theme="default-dark"
                     class="default-dark-theme working"
-                  >7</a>
+                    >7</a
+                  >
                 </li>
                 <li>
-                  <a href="javascript:void(0)" data-theme="green-dark" class="green-dark-theme">8</a>
+                  <a
+                    href="javascript:void(0)"
+                    data-theme="green-dark"
+                    class="green-dark-theme"
+                    >8</a
+                  >
                 </li>
                 <li>
-                  <a href="javascript:void(0)" data-theme="red-dark" class="red-dark-theme">9</a>
+                  <a
+                    href="javascript:void(0)"
+                    data-theme="red-dark"
+                    class="red-dark-theme"
+                    >9</a
+                  >
                 </li>
                 <li>
-                  <a href="javascript:void(0)" data-theme="blue-dark" class="blue-dark-theme">10</a>
+                  <a
+                    href="javascript:void(0)"
+                    data-theme="blue-dark"
+                    class="blue-dark-theme"
+                    >10</a
+                  >
                 </li>
                 <li>
-                  <a href="javascript:void(0)" data-theme="purple-dark" class="purple-dark-theme">11</a>
+                  <a
+                    href="javascript:void(0)"
+                    data-theme="purple-dark"
+                    class="purple-dark-theme"
+                    >11</a
+                  >
                 </li>
                 <li>
-                  <a href="javascript:void(0)" data-theme="megna-dark" class="megna-dark-theme">12</a>
+                  <a
+                    href="javascript:void(0)"
+                    data-theme="megna-dark"
+                    class="megna-dark-theme"
+                    >12</a
+                  >
                 </li>
               </ul>
             </div>
